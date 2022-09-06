79100bde3f7d4401a935f9b4e40e109932d54c06
rodrig1547 <91445938+rodrig1547@users.noreply.github.com>
2022-09-06 19:08:49 -0400

    Clases y Objetos

---
+++ Clases y Objetos/Clase_Lampara.py
@@ -0,0 +1,38 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "class Lampara:\n",
+    "    _ESTADOS = ['''\n",
+    "          .\n",
+    "     .    |    ,\n",
+    "      \\   '   /\n",
+    "       ` ,-. '\n",
+    "    --- (   ) ---\n",
+    "         \\ /\n",
+    "        _|=|_\n",
+    "       |_____|\n",
+    "    ''',\n",
+    "    '''\n",
+    "         ,-.\n",
+    "        (   )\n",
+    "         \\ /\n",
+    "        _|=|_\n",
+    "       |_____|\n",
+    "    ''']"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  },
+  "orig_nbformat": 4
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
